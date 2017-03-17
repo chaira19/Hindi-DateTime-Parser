395861a01d9ad773a9c26ad354e6e37984a8d91a
Chirayu Asati <chirayuasati19@gmail.com>
2017-03-17 20:31:24 +0530

    More Features

--- time.py
+++ time.py
@@ -2,6 +2,7 @@ import datetime
 from dateutil.relativedelta import *
 
 ## get the input
+## input or raw_input
 string = input("Give the text:")
 
 ## convert inpurt to lower case
@@ -33,7 +34,7 @@ def change_datetime ( c="0", y=0,  mt=0, w=0,  d=0,  h=0,  m=0,  s=0):
 
 
 
-
+## make separate functions for setting date and time and print -- if not provided the data
 ## give final date and time after parsing by setting date-time
 def set_datetime (y=0, mt=0, d=0, h=0, m=0, s=0, c="0"):
 
@@ -57,7 +58,7 @@ def set_datetime (y=0, mt=0, d=0, h=0, m=0, s=0, c="0"):
 	if c!="0":
 		a = a + " "
 		a = a + str(c)
-		print (a, "a")
+		#print (a, "a")
 
 	return a
 
@@ -65,11 +66,12 @@ def set_datetime (y=0, mt=0, d=0, h=0, m=0, s=0, c="0"):
 
 
 ## categories of words (dictionaries or associative arrays)
-Date = {"aaj":[0,"day"], "kal":[1,"day"], "parson":[2,"day"], "narson":[3,"day"], "dashak":[10,"year"], "tarikh":0, "mahine":[0,"months"], "hafte":[0,"week"], "din" or "divas" or "dino":[0,"day"], "saal" or "varsh":0, "monday":[MO,"weekday"], "tuesday":["TU","weekday"], "wednesday":["WE","weekday"], "thursday":["TH","weekday"], "friday":["FR","weekday"], "saturday":["SA","weekday"], "sunday":["SU","weekday"], "somvar":["MO","weekday"], "mangalvar":["TU","weekday"], "budhvar":["WE","weekday"], "guruvar":["TH","weekday"], "shukravar":["FR","weekday"], "shanivar":["SA","weekday"], "ravivar":["SU","weekday"], "january":[1,"month"], "february":[2,"month"], "march":[3,"month"], "april":[4,"month"], "may":[5,"month"], "june":[6,"month"], "july":[7,"month"], "august":[8,"month"], "september":[9,"month"], "october":[10,"month"], "november":[11,"month"], "december":[12,"month"]}
-Time = {"abhi":0, "baje":0, "baj":0, "ghante":0, "ghanta":1, "ghanton":1, "subah":0, "shaam":0, "dopahar":0, "raat":0, "minutes":0, "minute":1, "seconds":0, "second":1}
+# you cannot use or for strings, find another better way
+Date = {"aaj":[0,"day"], "kal":[1,"day"], "parson":[2,"day"], "narson":[3,"day"], "dashak":[10,"year"], "tarikh":0, "mahine":[0,"months"], "mahina":[0,"months"], "hafte":[0,"week"], "din":[0,"day"], "divas":[0,"day"], "dino":[0,"day"], "saal":0, "varsh":0, "monday":[MO,"weekday"], "tuesday":["TU","weekday"], "wednesday":["WE","weekday"], "thursday":["TH","weekday"], "friday":["FR","weekday"], "saturday":["SA","weekday"], "sunday":["SU","weekday"], "somvar":["MO","weekday"], "mangalvar":["TU","weekday"], "budhvar":["WE","weekday"], "guruvar":["TH","weekday"], "shukravar":["FR","weekday"], "shanivar":["SA","weekday"], "ravivar":["SU","weekday"], "january":[1,"month"], "february":[2,"month"], "march":[3,"month"], "april":[4,"month"], "may":[5,"month"], "june":[6,"month"], "july":[7,"month"], "august":[8,"month"], "september":[9,"month"], "october":[10,"month"], "november":[11,"month"], "december":[12,"month"]}
+Time = {"abhi":0, "baje":0, "baj":0, "ghante":0, "ghanta":1, "ghanton":1, "subah":0, "shaam":0, "sandhya":0, "dopahar":0, "raat":0, "minutes":0, "minute":1, "seconds":0, "second":1}
 DateTime = {"baad":[1,-1], "is":[0,1], "pahle":[-1,-1], "pichle":[-1,1], "agle":[1,1], "aadhe":[0.5,1], "aadha":[0.5,1], "dedh":[1.5,1], "dhaai":[2.5,1], "saade":[0.5,1]}
-Numbers = {"1" or "ek" or "pahla":[1,1], "2" or "do" or "doosra":[2,1], "3" or "teen" or "teesra":[3,1], "4" or "chaar" or "choutha":[4,1], "5" or "paanch" or "paanchva":[5,1], "6" or "chhe" or "chhata":[6,1], "7" or "saat" or "saatva":[7,1], "8" or "aath" or "aathva":[8,1], "9" or "nau" or "nahla":[9,1], "10" or "das" or "dasva":[10,1], "11" or "gyarah" or "gyarva":[11,1], "12" or "barah" or "barvan":[12,1], "13" or "terah":[13,1], "14" or "chaudah":[14,1], "15" or "pandrah":[15,1], "16" or "solah":[16,1], "17" or "satrah":[17,1], "18" or "atharah":[18,1], "19" or "unnish":[19,1], "20" or "bees":[20,1], "21" or "ikkish":[21,1], "22" or "baaish":[22,1], "23" or "teish":[23,1], "24" or "chaubish":[24,1], "25" or "pachhish":[25,1], "26" or "chhabish":[26,1], "27" or "sattaish":[27,1], "28" or "atthaish":[28,1], "29" or "untish":[29,1], "30" or "teesh":[30,1], "31" or "ikattish":[31,1]}
-Separators = {"se":0, "aur":0, "evam":0, "lekin":0, "par":0, "magar":0, "kintu":0, "parantu":0, "ya":0, "kyunki":0, "isliye":0}
+Numbers = {'1':[1,1], 'ek':[1,1], 'pahla':[1,1], '2':[2,1], 'do':[2,1], 'doosra':[2,1], '3':[3,1], 'teen':[3,1], 'teesra':[3,1], '4':[4,1], 'chaar':[4,1], 'choutha':[4,1], '5':[5,1], 'paanch':[5,1], 'paanchva':[5,1], '6':[6,1], 'chhe':[6,1], 'chhata':[6,1], '7':[7,1], 'saat':[7,1], 'saatva':[7,1], '8':[8,1], 'aath':[8,1], 'aathva':[8,1], '9':[9,1], 'nau':[9,1], 'nahla':[9,1], '10':[10,1], 'das':[10,1], 'dasva':[10,1], '11':[11,1], 'gyarah':[11,1], 'gyarva':[11,1], '12':[12,1], 'barah':[12,1], 'barvan':[12,1], '13':[13,1], 'terah':[13,1], '14':[14,1], 'chaudah':[14,1], '15':[15,1], 'pandrah':[15,1], '16':[16,1], 'solah':[16,1], '17':[17,1], 'satrah':[17,1], '18':[18,1], 'atharah':[18,1], '19':[19,1], 'unnish':[19,1], '20':[20,1], 'bees':[20,1], '21':[21,1], 'ikkish':[21,1], '22':[22,1], 'baaish':[22,1], '23':[23,1], 'teish':[23,1], '24':[24,1], 'chaubish':[24,1], '25':[25,1], 'pachhish':[25,1], '26':[26,1], 'chhabish':[26,1], '27':[27,1], 'sattaish':[27,1], '28':[28,1], 'atthaish':[28,1], '29':[29,1], 'untish':[29,1], '30':[30,1], 'teesh':[30,1], '31':[31,1], 'ikattish':[31,1]}
+Separators = {"se":0, "ke":0, "aur":0, "evam":0, "lekin":0, "par":0, "magar":0, "kintu":0, "parantu":0, "ya":0, "kyunki":0, "isliye":0}
 
 ## four stacks
 Stack_Date = []
@@ -319,7 +321,20 @@ for word in words:
 				Stack_Prev.append(word)
 				#print (word, "goes in Prev \n")
 
-	if words.index(word) == len(words)-1:	
+	if words.index(word) == len(words)-1:
+
+		if len(Stack_Prev)!=0:
+
+			temporary = Stack_Prev.pop()
+
+			if check==7:
+				Stack_Date.append(temporary)
+				#print (temporary, "goes in Date from Prev\n")
+
+			elif check==8:
+				Stack_Time.append(temporary)
+				#print (temporary, "goes in Time from Prev\n")	
+
 		if len(Stack_Date)!=0:
 			copy = Stack_Date[:]
 			Stack_of_Stacks.append(copy)
@@ -394,6 +409,8 @@ for date in Final['Date']:
 			if ('hafta' in date) or ('hafte' in date):
 				w = DateTime[list(x)[0]][0]
 
+			### Instead of using [0] use previous word for (aaj agle mahine pichle saal)
+
 		## if weekday
 		## and correspondigly agle is pichle set and pass weekday
 		## pass in format of WE(+1) where WE is not string
@@ -411,13 +428,13 @@ for date in Final['Date']:
 
 					next_word = date[date.index(word)+1]
 
-					if next_word == 'mahina' or 'mahine':
+					if (next_word == 'mahina') or (next_word == 'mahine'): ## check if next_word == 'mahina' or 'mahine' works or not
 						mt = Numbers[word][0]
 
-					elif next_word == 'saal' or 'varsh':
+					elif (next_word == 'saal') or (next_word == 'varsh'):
 						y = Numbers[word][0]
 
-					elif next_word == 'tarikh' or 'din':
+					elif (next_word == 'tarikh') or (next_word == 'din'):
 						d = Numbers[word][0]
 
 					elif (next_word.isdigit()) and (len(next_word)==4):
@@ -475,6 +492,9 @@ for time in Final['Time']:
 	x = set(time).intersection(DateTime.keys())
 	y = set(time).intersection(Numbers.keys())
 
+	if len(y) == 0:
+		y.add("1")
+
 	#print (x)
 	#print (y)
 
@@ -512,31 +532,34 @@ for time in Final['Time']:
 
 					next_word = time[time.index(word)+1]
 
-					if next_word == 'baj' or 'baje':
+					if (next_word == 'baj') or (next_word == 'baje'): ## baje is not working
 						h = Numbers[word][0]
+						print (h, "h")
 
-					elif next_word == 'minute' or 'minutes':
+					elif (next_word == 'minute') or (next_word == 'minutes'): ## or ke baad wale main dikkar hai
 						m = Numbers[word][0]
+						print (m, "m")
 
-					elif next_word == 'second' or 'seconds':
+					elif (next_word == 'second') or (next_word == 'seconds'):
 						s = Numbers[word][0]
+						print (s, "s")
 
-				if word == 'subah':
-					c = "AM"
+			if 'subah' in time:
+				c = "AM"
 
-				elif word == 'dopahar':
-					c = "PM"
+			elif 'dopahar' in time:
+				c = "PM"
 
-				elif word == ('shaam' or 'sandhya'):
-					c = "PM"
+			elif 'shaam' or 'sandhya' in time: ## sandhya to aa hi nahi raha
+				c = "PM"
 
-				elif word == 'raat' and (0 <= h < 6):
-					c = "AM"
+			elif (('raat' in time) and (0 <= h < 6)):
+				c = "AM"
 
-				elif word == 'raat' and (6 <= h < 12):
-					c = "PM"
+			elif (('raat' in time) and (6 <= h < 12)):
+				c = "PM"
 
-				print (c, "c")
+			print (c, "c")
 			Answer['Time'].append(set_datetime(0,0,0,h,m,s,c))
 
 		else:
@@ -547,17 +570,20 @@ for time in Final['Time']:
 			
 			for word in time:
 
+				if word == 'abhi':
+					duration.append(change_datetime("time"))
+
 				if word == 'subah':
 					duration.append("5 - 12 AM")
 
 				elif word == 'dopahar':
 					duration.append("12 - 5 PM")
 
-				elif word == 'shaam' or 'sandhya':
+				elif word == ('shaam' or 'sandhya'):
 					duration.append("5 - 12 PM")
 
 				elif word == 'raat':
-					duration.append("12 - 5 AM")
+					duration.append("10-12 PM & 12 - 5 AM")
 
 			Answer['Time'].append(duration)
  
