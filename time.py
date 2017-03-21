0c3bcbfc27c0b6964929cae0a17f81adc9b99a6e
Chirayu Asati <chirayuasati19@gmail.com>
2017-03-21 23:54:37 +0530

    functions.py

--- time.py
+++ time.py
@@ -1,8 +1,8 @@
 import data
 from data import *
 
-import datetime 
-from dateutil.relativedelta import *
+import functions
+from functions import *
 
 ## get the input
 ## input or raw_input
@@ -13,61 +13,6 @@ string = string.lower()
 
 ## split the input into words(list)
 words = string.split(" ")
-
-## give final date and time after parsing by changing current date-time
-def change_datetime ( c="0", y=0,  mt=0, w=0,  d=0,  h=0,  m=0,  s=0):
-
-	#mt = mt + 12*y
-	#d = d + 30*mt
-
-	now = datetime.datetime.now()
-	change = relativedelta( years =+ y, months =+ mt, weeks =+ w, days =+ d, hours =+ h, minutes =+ m, seconds =+ s)
-	#print (now + change)
-
-	if c == "date":
-		return (now + change).date()
-
-	elif c == "time":
-		return (now + change).time()
-
-## make separate date and time functions
-
-#def change_date (y=0, m=0, w=0, d=0):
-#def change_time (h=0, m=0, s=0):
-
-
-
-## make separate functions for setting date and time and print -- if not provided the data
-## give final date and time after parsing by setting date-time
-def set_datetime (y=0, mt=0, d=0, h=0, m=0, s=0, c="0"):
-
-	a = ""
-	if d!=0:
-		a = a + str(d) + "/"
-	if mt!=0:
-		a = a + str(mt) + "/"
-	if y!=0:
-		a = a + str(y)   
-
-	#a = a + " "
-
-	if h!=0:
-		a = a + str(h) + ":"
-	if m!=0:
-		a = a + str(m) + ":"
-	if s!=0:
-		a = a + str(s)
-
-	if c!="0":
-		a = a + " "
-		a = a + str(c)
-		#print (a, "a")
-
-	return a
-
-## make function for am/pm
-
-
 
 
 ## four stacks
