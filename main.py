03d15274583f4c889de1fdb143cb8712c4cbb923
was1499 <aniketw999@gmail.com>
2018-02-03 17:36:19 +0530

    added voice ip and op in main

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import voice
-from voice import *
-
 import stack
 from stack import *
 
@@ -9,8 +6,32 @@ from parser import *
 
 ## get the input
 ## input or raw_input
-#string = input("Give the text:")
-string = voice_input()
+ 
+import speech_recognition as sr
+ 
+# Record Audio
+r = sr.Recognizer()
+m = sr.Microphone()
+
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+ 
+# Speech recognition using Google Speech Recognition
+try:
+    # for testing purposes, we're just using the default API key
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # instead of `r.recognize_google(audio)`
+    string=r.recognize_google(audio)
+    #time.sleep(1)
+    
+    #stop_listening = r.listen_in_background(m, callback)
+   
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+print(string)
 
 ## convert inpurt to lower case
 string = string.lower()
@@ -24,16 +45,19 @@ Final = stack_algo(words)
 # empty function makes empty
 
 ## Get parsed dictionary from Final Dictionary
-Answer = parser.get_parsed(Final)
+Answer = get_parsed(Final={})
 
-print (Answer)
+import pyttsx3
+import time
+eng=pyttsx3.init()
+eng.say(Answer)
+eng.runAndWait()
 
-emails = get_emails(words)
+emails = get_emails_parsed(words)
 
 if len(emails)!=0:
 
-	print (emails)
-
+        print (emails)
 
 
 
